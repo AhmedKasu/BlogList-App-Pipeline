676660126dbdb05d2ea3c5e8ec8a2569b6d70c9b
Ahmed Kasu <ankassu@gmail.com>
2023-06-21 16:14:55 +0300

    reconfigured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,38 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
     node: true,
-    jest: true,
+    es6: true,
+    'jest/globals': true,
   },
-  extends: 'eslint:recommended',
-  overrides: [],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+    'plugin:jest/recommended',
+  ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  parser: 'babel-eslint',
+  plugins: ['react', 'jest', 'cypress'],
+  settings: {
+    react: {
+      version: '^18.2.0',
+    },
+  },
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'error',
+    'react/prop-types': 0,
   },
-};
+}
