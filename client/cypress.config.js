284ea1c84eaab46d8178332b32b4ba72acfae174
Ahmed Kasu <ankassu@gmail.com>
2023-06-28 16:30:55 +0300

    configured cypress dashboard & recording

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'xbu615',
   e2e: {
     defaultCommandTimeout: 15000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
