7ebcd1cd206ac127f51e9b95b4b305ca1fe1ff8d
Ahmed Kasu <ankassu@gmail.com>
2023-06-29 14:36:18 +0300

    http health & rollback check back to success

--- controllers/healthCheck.js
+++ controllers/healthCheck.js
@@ -2,13 +2,11 @@ const statusCheck = require('express').Router()
 const versionCheck = require('express').Router()
 
 statusCheck.get('/', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.status(200).send('ok')
 })
 
 versionCheck.get('/', (req, res) => {
-  res.send('2')
+  res.send('3')
 })
 
 module.exports = { statusCheck, versionCheck }
