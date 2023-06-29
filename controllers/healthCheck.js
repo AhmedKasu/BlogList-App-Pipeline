9496f2c8960ae7c76f9a95fc92b086a9ab5cf111
Ahmed Kasu <ankassu@gmail.com>
2023-06-29 14:24:08 +0300

    added http health check & rollback failure test

--- controllers/healthCheck.js
+++ controllers/healthCheck.js
@@ -2,11 +2,13 @@ const statusCheck = require('express').Router()
 const versionCheck = require('express').Router()
 
 statusCheck.get('/', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.status(200).send('ok')
 })
 
 versionCheck.get('/', (req, res) => {
-  res.send('1')
+  res.send('2')
 })
 
 module.exports = { statusCheck, versionCheck }
